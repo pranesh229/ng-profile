a9f50f043ef3716915f9fa94d46858c4164b9e8c
hanumanp <pranesh.hanumanth@avon.com>
2018-06-23 01:29:08 -0400

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { ShortProfileComponent } from './short-profile/short-profile.component';
+import { MatGridListModule } from '@angular/material/grid-list';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ShortProfileComponent],
+  imports: [BrowserModule, MatGridListModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/short-profile/short-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-short-profile',
+  templateUrl: './short-profile.component.html',
+  styleUrls: ['./short-profile.component.scss']
+})
+export class ShortProfileComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
