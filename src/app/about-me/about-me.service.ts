65b61f8b8580d29952bb4a269360503d79fa449b
hanumanp <pranesh.hanumanth@avon.com>
2018-07-20 15:10:03 -0400

    Lazyloading and caching response

--- src/app/about-me/about-me.service.ts
+++ src/app/about-me/about-me.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { catchError, map } from 'rxjs/operators';
+import { catchError, map, startWith } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -8,18 +9,29 @@ export class AboutMeService {
   serviceURL =
     'https://cdn.contentful.com/spaces/hgjyi5lurih3/environments/master/entries/557E2oqinYcsgIi4EegKaW' +
     '?access_token=b3b3b518c7649017f733b6eaf64952902296ac159f37fc1a30223666fe1c9412';
-  aboutmeData: AboutMeObj;
+  aboutmeData;
   constructor(private http: HttpClient) {}
   getAboutMeData() {
-    return this.http.get<AboutMeObj>(this.serviceURL).pipe(
-      map(respone => {
-        this.aboutmeData = respone;
-      }),
-      catchError(error => {
-        console.log(error);
-        return error;
-      })
-    );
+    if (localStorage['aboutmeData']) {
+      return of(JSON.parse(localStorage['aboutmeData'])).pipe(
+        map(response => {
+          this.aboutmeData = response;
+          return response;
+        })
+      );
+    } else {
+      return this.http.get<AboutMeObj>(this.serviceURL).pipe(
+        map(response => {
+          this.aboutmeData = response;
+          localStorage['aboutmeData'] = JSON.stringify(response);
+          return response;
+        }),
+        catchError(error => {
+          console.log(error);
+          return error;
+        })
+      );
+    }
   }
   getAboutMeDescription(): string {
     return this.aboutmeData.fields.description;
